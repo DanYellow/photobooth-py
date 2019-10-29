24104ac9468de0b1144883d610c7b67c1464e933
Danielo JEAN-LOUIS <jeanlouis.danielo@sfr.fr>
2019-10-29 13:25:33 +0100

    wip

--- test.py
+++ test.py
@@ -8,17 +8,13 @@ from PIL import Image
 
 import io
 
-
-
 import time
 import gphoto2 as gp
 
-def capture_images():
+def capture_images(camera):
     logging.basicConfig(
         format='%(levelname)s: %(name)s: %(message)s', level=logging.WARNING)
-    NB_MAX_PHOTOS = 2
-    camera = gp.Camera()
-    camera.init()
+    NB_MAX_PHOTOS = 1
 
     for x in range(0, NB_MAX_PHOTOS):
         ts = time.time()
@@ -32,7 +28,7 @@ def capture_images():
         print('Copying image to', target)
         camera_file = camera.file_get(
             file_path.folder, file_path.name, gp.GP_FILE_TYPE_NORMAL)
-        camera_file.save(target)
+
         file_data = gp.check_result(gp.gp_file_get_data_and_size(camera_file))
 
         foo = io.BytesIO(file_data)
@@ -44,16 +40,28 @@ def capture_images():
         im = im.resize((basewidth, hsize), Image.ANTIALIAS)
 
         im.save(f"./_tmp/{ts}.jpg")
-
-        # os.remove(target)
-
+        
     camera.exit()
 
 def main():
-    capture_images()
+    while True:
+        try:
+            context = gp.Context()
+            camera = gp.Camera()
+            camera.init(context)
+        except gp.GPhoto2Error as ex:
+            print('Please connect and switch on your camera')
+            if ex.code == gp.GP_ERROR_MODEL_NOT_FOUND:
+                # no camera, try again in 2 seconds
+                time.sleep(2)
+                continue
+            # some other error we can't handle here
+            raise
+        # operation completed successfully so exit loop
+        break
 
-   
-    
+    capture_images(camera)
+
     return 0
 
 if __name__ == "__main__":
