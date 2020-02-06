e15ac3338d3e94bd6e0fa80379b3550746bc9944
DanYellow <jeanlouis.danielo@sfr.fr>
2020-02-06 23:59:57 +0100

    Add help button

--- software/main.py
+++ software/main.py
@@ -1,5 +1,6 @@
 import tkinter.ttk as ttk
 import tkinter as tk
+import tkinter.font as tkFont
 import glob, os, qrcode
 
 from classes.Gallery import Gallery
@@ -14,6 +15,23 @@ class PhotoboothApplication(ttk.Frame):
         self['style'] = 'App.TFrame'
 
         self.ROOT_DIR = f"{os.path.dirname(os.path.abspath(__file__))}/.."
+
+        self.translation = {
+            'fr': {
+                'take_pict': "COMMENCER",
+                'help': "aide".upper(),
+                'print': 'Imprimer',
+                'cancel': "Annuler",
+                'not_print': "Ne pas imprimer",
+                'last_collage': "Dernier collage",
+                'ready': 'Prêt ?',
+                'take_another_one': "Prendre une autre photo",
+                'loading': "Chargement",
+                'printing': "Impression lancée",
+                'access_gallery': "Accéder à la galerie",
+                'link_to_gallery': "ou \nraspberrypi.local",
+            }
+        }
 
         self.configure_gui()
         self.create_widgets()
@@ -39,17 +57,69 @@ class PhotoboothApplication(ttk.Frame):
         os.popen(f"mkdir -p {full_dir} && mkdir -p {collages_dir}")
 
     def create_home_screen(self):
-        home_screen_style = ttk.Style()
-        home_screen_style.configure('HomeScreen.TFrame', background="red")
-        self.home_screen = ttk.Frame(self)
-        self.home_screen['style'] = 'HomeScreen.TFrame'
-        self.home_screen.pack(fill="both", expand=True)
+        main_container = tk.Frame(self, bg = "white")
+        main_container.pack(fill="both", expand=True)
 
+        navigation_style = ttk.Style()
+        navigation_style.configure('HomeScreenBtnsContainer.TFrame', background="#262727")
+        navigation = ttk.Frame(
+            main_container,
+            width="360",
+            height="520",
+            style='HomeScreenBtnsContainer.TFrame'
+        )
+        # navigation.configure(width=500, height=450)
 
-        gallery_bg = Gallery(self.home_screen, self.parent)
-        gallery_bg.pack(fill="both", expand=True)
+        navigation.pack(pady=30)
+        navigation.pack_propagate(0)
+        
+
+        btns_container = tk.Frame(
+            navigation,
+        )
+        btns_container.pack(side="top", fill="x", expand=True)
+
+        btns_container_font_style = tkFont.Font(family='DejaVu Sans Mono', size=15)
+        start_btn = tk.Button(
+            btns_container,
+            height = 2,
+            text = self.translation['fr']['take_pict'],
+            # command = self.actions['take_pictures'],
+            background="#a1d4f0",
+            highlightthickness=2, 
+            highlightcolor="red", 
+            highlightbackground="#8e9ae9", 
+            borderwidth=0,
+            fg="white",
+            font=btns_container_font_style
+        )
+        start_btn.pack(fill="x", expand=True, padx=10, pady=5)
+
+        help_btn = tk.Button(
+            btns_container,
+            height = 2,
+            text = self.translation['fr']['help'],
+            # command = self.actions['take_pictures'],
+            background="#e67e22",
+            highlightthickness=2, 
+            highlightcolor="red", 
+            highlightbackground="#f2a560", 
+            borderwidth=0,
+            fg="white",
+            font=btns_container_font_style
+        )
+        help_btn.pack(fill="x", expand=True, padx=10, pady=5)
+
+        gallery_bg = Gallery(main_container, self.parent)
+        gallery_bg.place(
+            relx=0.5, rely=0.5,
+            anchor="center",
+            relheight=1.0, relwidth=1.0
+        )
         gallery_bg.lower()
         # qrc_frame = ttk.Frame(self.home_screen, bg=self['bg'])
+
+        
 
 if __name__ == "__main__":
     root = tk.Tk()
