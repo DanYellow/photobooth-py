f840b866f972769ed736c9d72d9485e1547a6f57
DanYellow <jeanlouis.danielo@sfr.fr>
2020-02-06 21:09:00 +0100

    Improve Gallery component

--- software/classes/Gallery.py
+++ software/classes/Gallery.py
@@ -1,24 +1,23 @@
 import glob
 import os
+import tkinter as tk
 
 from math import ceil
-from tkinter import Frame, Label
 from PIL import Image, ImageTk, ImageFile, ImageFilter
 
-class Gallery(Frame):
+class Gallery(tk.Frame):
     def load(self):
-
-        COLUMNS = ceil(self.master.winfo_screenwidth() / 150)
+        COLUMNS = ceil(self.root.winfo_width() / 150)
         image_count = 0
 
-        for infile in glob.glob(os.path.join("_tmp/", '*.JPG')):
+        for infile in glob.glob(os.path.join("../_tmp/", '*.JPG')):
             image_count += 1
             r, c = divmod(image_count - 1, COLUMNS)
 
             img = Image.open(infile)
             resized = img.resize((150, 100), Image.ANTIALIAS).filter(ImageFilter.GaussianBlur(2))
             tkimage = ImageTk.PhotoImage(resized)
-            img_container = Label(self, image=tkimage)
+            img_container = tk.Label(self, image=tkimage)
             img_container.image = tkimage
 
             img_container.grid(row=r, column=c)
@@ -26,11 +25,14 @@ class Gallery(Frame):
             self.grid_rowconfigure(r, pad=0)
             self.grid_columnconfigure(c, pad=0)
 
-    def __init__(self, master=None):
+    def __init__(self, master=None, root=None):
         bgc = 'white'
-        Frame.__init__(self, master, image = None, bg = bgc)
+        tk.Frame.__init__(self, master, image = None, bg = bgc)
         self['bg'] = bgc
         self.master = master
+        self.root = root
+
+        root.update()
 
         self.load()
         
--- software/main.py
+++ software/main.py
@@ -1,15 +1,22 @@
+import tkinter.ttk as ttk
 import tkinter as tk
 import glob, os, qrcode
 
-class PhotoboothApplication(tk.Frame):
+from classes.Gallery import Gallery
+
+
+class PhotoboothApplication(ttk.Frame):
     def __init__(self, parent, *args, **kwargs):
         self.parent = parent
-        tk.Frame.__init__(self, self.parent, *args, **kwargs)
-        self['bg'] = 'black'
+        ttk.Frame.__init__(self, self.parent, *args, **kwargs)
+        main_style = ttk.Style()
+        main_style.configure('App.TFrame', background="white")
+        self['style'] = 'App.TFrame'
 
         self.ROOT_DIR = f"{os.path.dirname(os.path.abspath(__file__))}/.."
 
         self.configure_gui()
+        self.create_widgets()
         self.setup_files_and_folders()
 
     def configure_gui(self):
@@ -21,17 +28,28 @@ class PhotoboothApplication(tk.Frame):
         self.parent.geometry(f"600x800")
         self.parent.resizable(False, False)
 
-    def setup_files_and_folders():
-        os.chdir(ROOT_DIR)
-        full_dir = f"{ROOT_DIR}/_tmp/full"
-        collages_dir = f"{ROOT_DIR}/_tmp/collages"
+    def create_widgets(self):
+        self.create_home_screen()
+
+    def setup_files_and_folders(self):
+        os.chdir(self.ROOT_DIR)
+        full_dir = f"{self.ROOT_DIR}/_tmp/full"
+        collages_dir = f"{self.ROOT_DIR}/_tmp/collages"
 
         os.popen(f"mkdir -p {full_dir} && mkdir -p {collages_dir}")
 
-    def create_home_screen():
-        self.home_screen = tk.PanedWindow(self, orient = "vertical", bg = self['bg'])
+    def create_home_screen(self):
+        home_screen_style = ttk.Style()
+        home_screen_style.configure('HomeScreen.TFrame', background="red")
+        self.home_screen = ttk.Frame(self)
+        self.home_screen['style'] = 'HomeScreen.TFrame'
+        self.home_screen.pack(fill="both", expand=True)
 
-        qrc_frame = tk.Frame(self.home_screen, bg=self['bg'])
+
+        gallery_bg = Gallery(self.home_screen, self.parent)
+        gallery_bg.pack(fill="both", expand=True)
+        gallery_bg.lower()
+        # qrc_frame = ttk.Frame(self.home_screen, bg=self['bg'])
 
 if __name__ == "__main__":
     root = tk.Tk()
